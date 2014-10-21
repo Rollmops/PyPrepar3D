22da0fd7abe59d543ab323e53af4a490f031d615
Rollmops <etuerke@googlemail.com>
2014-10-21 20:09:49 +0200

    cosmetics

--- src/simconnect_general.cpp
+++ src/simconnect_general.cpp
@@ -1,51 +1,42 @@
 #include "module_simconnect.hpp"
 #include <boost/function.hpp>
 
-namespace prepar3d
-{
+namespace prepar3d {
 
 using namespace boost::python;
 namespace _internal {
 static object *__callback__;
+void CALLBACK __caller__(SIMCONNECT_RECV* pData, DWORD cbData, void *pContext) {
+	_internal::__callback__->operator ()(pData, cbData,
+			handle<>(PyCObject_FromVoidPtr(pContext, NULL)));
 }
 
-tuple simconnect_open(PCSTR szName, HWND hWnd,
-		DWORD UserEventWin32, HANDLE hEventHandle, DWORD ConfigIndex)
-{
+}
+
+tuple simconnect_open(PCSTR szName, HWND hWnd, DWORD UserEventWin32,
+		HANDLE hEventHandle, DWORD ConfigIndex) {
 	HANDLE phSimConnect;
-	const HRESULT result = SimConnect_Open(&phSimConnect, szName, hWnd, UserEventWin32,
-			hEventHandle, ConfigIndex);
+	const HRESULT result = SimConnect_Open(&phSimConnect, szName, hWnd,
+			UserEventWin32, hEventHandle, ConfigIndex);
 
-	return make_tuple(result, handle<>(borrowed(PyCObject_FromVoidPtr(phSimConnect, NULL))));
+	return make_tuple(result,
+			handle<>(borrowed(PyCObject_FromVoidPtr(phSimConnect, NULL))));
 }
 
-HRESULT simconnect_close(PyObject *handle)
-{
+HRESULT simconnect_close(PyObject *handle) {
 	HRESULT ret = E_FAIL;
 	HANDLE han = PyCObject_AsVoidPtr(handle);
-	if( han != NULL ) {
+	if (han != NULL) {
 		ret = SimConnect_Close(han);
 	}
 	return ret;
 }
 
-void CALLBACK MyDispatchProcDLL(SIMCONNECT_RECV* pData, DWORD cbData, void *pContext)
-{
-	_internal::__callback__->operator ()( pData, cbData, handle<>(PyCObject_FromVoidPtr(pContext, NULL)));
-}
-
-enum EVENT_ID
-{
-	EVENT_ADDED_AIRCRAFT
-};
-
-HRESULT simconnect_calldispatch(PyObject *handle, object callback, PyObject *content)
-{
+HRESULT simconnect_calldispatch(PyObject *handle, object callback,
+		PyObject *content) {
 	_internal::__callback__ = &callback;
-//	boost::function<void(SIMCONNECT_RECV* , DWORD , void* )> f(callback);
-//	DispatchProc fp = *f.target<DispatchProc>();
-	SimConnect_SubscribeToSystemEvent(PyCObject_AsVoidPtr(handle), EVENT_ADDED_AIRCRAFT, "Paused");
-	return SimConnect_CallDispatch(PyCObject_AsVoidPtr(handle), MyDispatchProcDLL, NULL);
+	return SimConnect_CallDispatch(PyCObject_AsVoidPtr(handle),
+			_internal::__caller__, NULL);
 }
 
 } // end namespace prepar3d

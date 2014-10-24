f9a4aa9ab905fe757817046bd8fa5d06838a803d
Erik Türke <etuerke@googlemail.com>
2014-10-24 15:48:14 +0200

    cosmetics

--- src/simconnect/dispatch_receiver.hpp
+++ src/simconnect/dispatch_receiver.hpp
@@ -13,37 +13,40 @@ namespace prepar3d {
 namespace simconnect {
 
 class DispatchReceiver {
-	typedef boost::function<object (SIMCONNECT_RECV*) > FunctionType;
+	typedef boost::function<object(SIMCONNECT_RECV*)> FunctionType;
 public:
-	DispatchReceiver() {init();}
-	DispatchReceiver(PyObject *handle) : _handle(handle) {init();}
+	DispatchReceiver() {
+		init();
+	}
+	DispatchReceiver(PyObject *handle) :
+			_handle(handle) {
+		init();
+	}
 
 	void registerID(SIMCONNECT_RECV_ID id);
 
 	tuple getNextDispatch() {
 		return getNextDispatchForHandle(_handle);
 	}
-	tuple getNextDispatchForHandle(PyObject *handle) ;
+	tuple getNextDispatchForHandle(PyObject *handle);
 private:
 
 	void init();
 
-	std::map<SIMCONNECT_RECV_ID, FunctionType > _functionMap;
-	std::map<SIMCONNECT_RECV_ID, FunctionType > _registeredIdList;
+	std::map<SIMCONNECT_RECV_ID, FunctionType> _functionMap;
+	std::map<SIMCONNECT_RECV_ID, FunctionType> _registeredIdList;
 
 	PyObject *_handle;
 };
 
 namespace _internal {
 template<typename TYPE>
-object castToRecvType( SIMCONNECT_RECV * const& source)
-{
-	return object((TYPE *)source);
+object castToRecvType(SIMCONNECT_RECV * const & source) {
+	return object((TYPE *) source);
 }
 
 } // end namspace _internal
 } // end namespace simconnect
 } // end namespace prepar3d
-
 
 #endif // __PREPAR3D_SIMCONNECT_DISPATCH_RECEIVER_HPP_

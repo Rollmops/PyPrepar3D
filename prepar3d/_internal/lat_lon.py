c73a9458828f5b75b28409810782bf40e238fd22
Erik Türke <etuerke@googlemail.com>
2014-11-20 19:55:12 +0100

    - added tests for LatLon class

--- prepar3d/_internal/lat_lon.py
+++ prepar3d/_internal/lat_lon.py
@@ -1,8 +1,10 @@
 import re
+from math import radians, cos, sin, asin, sqrt
 
 class LatLon(object):
     
     _MULTIPLICATOR = {'N':1, 'S':-1, 'W':-1, 'E':1}
+    _RADIUS_EARTH = 6371
 
     # N37 36' 26.00"
     _DEG_MIN_SEC_LAT_REGEX = re.compile(r'^(N|S)(\d+)\s+(\d+).\s+(\d{1,2}\.\d+).\s*$', re.IGNORECASE)
@@ -11,6 +13,8 @@ class LatLon(object):
     def __init__(self, lat_lon):
         self._lat = lat_lon[0]
         self._lon = lat_lon[1]
+        self._lat_rad = radians(lat_lon[0])
+        self._lon_rad = radians(lat_lon[1])
         
     @staticmethod
     def from_deg_min_sec(lat_lon):
@@ -22,3 +26,27 @@ class LatLon(object):
                           (int(mlon.group(2)) + int(mlon.group(3)) / 60 + float(mlat.group(4)) / 3600.0) * LatLon._MULTIPLICATOR[mlon.group(1)]))
         else:
             return None
+
+    
+    def __eq__(self, other):
+        return self._lat == other._lat and self._lon == other._lon
+    
+    def distance(self, other):
+        dlat = other._lat_rad - self._lat_rad
+        dlon = other._lon_rad - self._lon_rad
+        
+        a = sin(dlat/2)**2 + cos(self._lat_rad) * cos(other._lat_rad) * sin(dlon/2)**2
+        c = 2 * asin(sqrt(a)) 
+
+        # 6367 km is the radius of the Earth
+        km = LatLon._RADIUS_EARTH * c
+        return abs(km)
+    
+    def move(self, meters, heading):
+        pass
+        
+        
+        
+    
+        
+        
--- tests/_internal/lat_lon_test.py
+++ tests/_internal/lat_lon_test.py
@@ -4,19 +4,34 @@ Created on Nov 19, 2014
 @author: Erik
 '''
 import unittest
-import timeit
 from prepar3d._internal.lat_lon import LatLon
 
 class Test(unittest.TestCase):
 
 
-    def test_lat_lon(self):
+    def test_lat_lon_from_deg_min_sec(self):
+        lat_lon1 = LatLon.from_deg_min_sec(('N37 36. 26.00.', 'W122 22. 49.59.'))
         
-        print(timeit.timeit("LatLon.from_deg_min_sec(('N37 36. 26.00.', 'W122 22. 49.59.'))", setup='from prepar3d._internal.lat_lon import LatLon'))
+        self.assertTrue(lat_lon1 is not None)
+        self.assertAlmostEqual(lat_lon1._lat, 37.607222, places=5)
+        self.assertAlmostEqual(lat_lon1._lon, -122.373888, places=5)
+    
+    def test_lat_lon_eq(self):
+        lat_lon1 = LatLon.from_deg_min_sec(('N37 36. 26.00.', 'W122 22. 49.59.'))
+        lat_lon2 = LatLon.from_deg_min_sec(('N37 36. 26.00.', 'E122 22. 49.59.'))
         
-        print( LatLon.from_deg_min_sec(('N37 36. 26.00.', 'W122 22. 49.59.'))._lon)
-        print( LatLon.from_deg_min_sec(('N37 36. 26.00.', 'W122 22. 49.59.'))._lat)
+        self.assertEqual(lat_lon1, lat_lon1)
+        self.assertNotEqual(lat_lon1, lat_lon2)
+        
+
+    def test_distance(self):
+        lat_lon1 = LatLon.from_deg_min_sec(('N37 36. 26.00.', 'W13 22. 49.59.'))
+        lat_lon2 = LatLon.from_deg_min_sec(('N34 36. 26.00.', 'W122 22. 49.59.'))
+
+        self.assertAlmostEqual(lat_lon1.distance(lat_lon2), 9149.306289152, 6)
+        self.assertAlmostEqual(lat_lon2.distance(lat_lon1), 9149.306289152, 6)
+        self.assertEqual(lat_lon1.distance(lat_lon1), 0)
 
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
+    # import sys;sys.argv = ['', 'Test.testName']
     unittest.main()

40d2b989a5da00cafb75bc2b50f1778b8fbb7189
Erik Tuerke <etuerke@googlemail.com>
2015-12-02 20:03:53 +0100

    add radius data type struct

---
+++ src/simconnect/radius_data_type.hpp
@@ -0,0 +1,25 @@
+/*
+ * radius_data_type.hpp
+ *
+ *  Created on: 02.12.2015
+ *      Author: erik_
+ */
+
+#ifndef SRC_SIMCONNECT_RADIUS_DATA_TYPE_HPP_
+#define SRC_SIMCONNECT_RADIUS_DATA_TYPE_HPP_
+
+#include "SimConnect.h"
+
+namespace pyprepar3d {
+
+struct RadiusDataType {
+
+	SIMCONNECT_DATA_DEFINITION_ID dataDefinitionID;
+	DWORD radius;
+	SIMCONNECT_SIMOBJECT_TYPE simObjectType;
+
+};
+
+} // end namespace pyprepar3d
+
+#endif /* SRC_SIMCONNECT_RADIUS_DATA_TYPE_HPP_ */

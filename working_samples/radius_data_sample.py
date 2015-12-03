0cd1fb7385e6b704ddf490fd50e83ef05942b8e8
Erik Tuerke <etuerke@googlemail.com>
2015-12-03 10:01:50 +0100

    adapted radius data sample

--- working_samples/radius_data_sample.py
+++ working_samples/radius_data_sample.py
@@ -1,34 +1,26 @@
-# import sys
-# import re
-# from prepar3d import 
-# from prepar3d.event import DataEvent
-# 
-# class RollEvent(DataEvent):
-#     
-#     def __init__(self):
-#         self._re = re
-#         request_data = [prepar3d.SimulationVariable(variable) for variable in ['title', 'STRUCT LATLONALT']]
-#         super(RollEvent, self).__init__(request_data, at_sim_start=False)
-#         
-#     def event(self, data):
-#         print('huhu')
-#         print(self._re._data)
-# 
-# 
-# if __name__ == '__main__':
-#     
-#     try:
-#         # the connection closes either when it gets destroyed or if an SIMCONNECT_RECV_ID_QUIT event is received
-#         prepar3d.Connection().open('Data Request Sample', auto_close=True)
-#     except prepar3d.OpenConnectionException:
-#         print('Uups! Is Prepar3d running?')
-#         sys.exit(1) 
-#         
-#     print('Connected to Prepar3d!')
-#     
-#     request_data = [prepar3d.SimulationVariable(variable) for variable in ['title', 'STRUCT LATLONALT']]
-#     
-#     r=RollEvent(prepar3d.RadiusData(request_data, radius=1000))
-#     
-#     prepar3d.EventListener().listen()
-#     
+import sys
+import re
+from prepar3d.connection import Connection
+from prepar3d.event import RadiusDataEvent
+ 
+class RollEvent(RadiusDataEvent):
+     
+    def __init__(self):
+        variables = [prepar3d.SimulationVariable(variable) for variable in ['title', 'STRUCT LATLONALT']]
+        super().__init__(variables,
+                         radius=1000,
+                         object_type=SIMCONNECT_SIMOBJECT_TYPE.SIMCONNECT_SIMOBJECT_TYPE_AIRCRAFT,
+                         at_sim_start=False)
+         
+    def event(self, data):
+        print('huhu')
+        print(data)
+ 
+ 
+if __name__ == '__main__':
+    with Connection('Data Request Sample').open() as connection:
+                 
+        print('Connected to Prepar3d!')
+        
+        connection.subscribe(RollEvent())
+        connection.listen()
